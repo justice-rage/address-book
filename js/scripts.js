4d7c25e1c8e0644f28f3459cffa348e709bc8686
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-25 20:02:18 -0800

    add user input and info show and hide logic
    Co-authored-by: Deya Card <thedeyacard@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-  $(".clickable-js").click(function() {
-    $("#js-def-showing").toggle();
+  // Hide & Show User Input Logic
+  $(".clickable-info").click(function() {
+    $("#user-info-showing").toggle();
     $("clickable").removeClass();
-    $("#js-def-hidden").show();
+    $("#user-info-hidden").show();
   });
 
-  $(".clickable-string").click(function() {
-    $("#string-def-showing").toggle();
-    $("clickable").removeClass();
-    $("#string-def-hidden").show();
+  // User Input Logic
+  $("#formInfo").submit(function(event) {
+    event.preventDefault();
+    const person1Input = $("input#person1").val();
+    const address1Input = $("input#address1").val();
+    const phone1Input = $("input#phone1").val(); 
+    $(".person1").text(person1Input);
+    $(".address1").text(address1Input);
+    $(".phone1").text(phone1Input);
   });
 });
